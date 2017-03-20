9d996766f7082c2a89689a831a84b59f3b50c039
Marcin Bielecki <marcin.bielecki@eracent.com>
2017-03-20 11:29:52 +0100

    translations added

--- MMM-SmokedMirror.js
+++ MMM-SmokedMirror.js
@@ -1,194 +1,201 @@
 Module.register('MMM-SmokedMirror', {
     defaults: {
-		showLocation: true,
-		showValues: true,
-		updateInterval: 30,
-		animationSpeed: 1000,
-		nowCast: false,
-    },
-	start: function(){
-		Log.info('Starting module: ' + this.name);
-		
-		//type of pollution
-		switch(this.config.pollutionType) {
-			case 'PM10':
-				this.config.pollutionColumn = 0;
-				this.config.pollutionNorm = 50;
-				break;
-			case 'PM2,5':
-				this.config.pollutionColumn = 1;
-				this.config.pollutionNorm = 25;
-				break;
-			case 'O3':
-				this.config.pollutionColumn = 2;
-				this.config.pollutionNorm = 120;
-				break;
-			case 'NO2':
-				this.config.pollutionColumn = 3;
-				this.config.pollutionNorm = 200;
-				break;
-			case 'SO2':
-				this.config.pollutionColumn = 4;
-				this.config.pollutionNorm = 125;
-				break;
-			case 'C6H6':
-				this.config.pollutionColumn = 5;
-				this.config.pollutionNorm = 5;
-				break;
-			case 'CO':
-				this.config.pollutionColumn = 6;
-				this.config.pollutionNorm = 10;
-				break;
-		}
-		switch(this.config.pollutionType) {
-			case 'PM10':
-			case 'PM2,5':
-			case 'O3':
-			case 'NO2':
-			case 'SO2':
-			case 'C6H6':
-				this.config.units = 'µg/m³'
-				break;
-			case 'CO':
-				this.config.units = 'mg/m³'
-				break;
-		}
+    showLocation: true,
+    showValues: false,
+    updateInterval: 30,
+    animationSpeed: 1000,
+    nowCast: false,
+    showUnits: false,
+  },
+  start: function(){
+    Log.info('Starting module: ' + this.name);
     
-		// uri to gather data
-		this.config.url = 'http://powietrze.gios.gov.pl/pjp/current/station_details/table/' + this.config.stationID + '/' + (this.config.nowCast ? 2 : 1) + '/0'
-		this.config.yql = 'SELECT * FROM html WHERE url="' + this.config.url + '" AND xpath=\'//div[@class="container"]//div[@class="row"]//div[@class="table-responsive"]//table//tbody//tr' + (this.config.nowCast ? '\'|sort(field="th", descending="true")' : '//td[' + (1 + this.config.pollutionColumn) + ']\'|truncate(count=24)')
-		
-		// load data
-		this.load();
+    //type of pollution
+    switch(this.config.pollutionType) {
+      case 'PM10':
+        this.config.pollutionColumn = 0;
+        this.config.pollutionNorm = 50;
+        break;
+      case 'PM2,5':
+        this.config.pollutionColumn = 1;
+        this.config.pollutionNorm = 25;
+        break;
+      case 'O3':
+        this.config.pollutionColumn = 2;
+        this.config.pollutionNorm = 120;
+        break;
+      case 'NO2':
+        this.config.pollutionColumn = 3;
+        this.config.pollutionNorm = 200;
+        break;
+      case 'SO2':
+        this.config.pollutionColumn = 4;
+        this.config.pollutionNorm = 125;
+        break;
+      case 'C6H6':
+        this.config.pollutionColumn = 5;
+        this.config.pollutionNorm = 5;
+        break;
+      case 'CO':
+        this.config.pollutionColumn = 6;
+        this.config.pollutionNorm = 10;
+        break;
+    }
+    switch(this.config.pollutionType) {
+      case 'PM10':
+      case 'PM2,5':
+      case 'O3':
+      case 'NO2':
+      case 'SO2':
+      case 'C6H6':
+        this.config.units = 'µg/m³'
+        break;
+      case 'CO':
+        this.config.units = 'mg/m³'
+        break;
+    }
     
-		// schedule refresh
-		setInterval(
-			this.load.bind(this),
-			this.config.updateInterval * 60 * 1000);
-	},
-	load: function(){
+    // uri to gather data
+    this.config.url = 'http://powietrze.gios.gov.pl/pjp/current/station_details/table/' + this.config.stationID + '/' + (this.config.nowCast ? 2 : 1) + '/0'
+    this.config.yql = 'SELECT * FROM html WHERE url="' + this.config.url + '" AND xpath=\'//div[@class="container"]//div[@class="row"]//div[@class="table-responsive"]//table//tbody//tr' + (this.config.nowCast ? '\'|sort(field="th", descending="true")' : '//td[' + (1 + this.config.pollutionColumn) + ']\'|truncate(count=24)')
+    
+    // load data
+    this.load();
+    
+    // schedule refresh
+    setInterval(
+      this.load.bind(this),
+      this.config.updateInterval * 60 * 1000);
+  },
+  load: function(){
 
-		if(!this.data.location) {
-			this.loadLocation();
-		}
+    if(!this.data.location) {
+      this.loadLocation();
+    }
 
-		var self = this;
+    var self = this;
 
-		YUI().use('node', 'event', 'yql', function(Y) {
-			Y.YQL(self.config.yql, function(response) {
-				if(response.error) {
-					Log.error(response.error.description)
-				}
-				else {
-					var pollution
+    YUI().use('node', 'event', 'yql', function(Y) {
+      Y.YQL(self.config.yql, function(response) {
+        if(response.error) {
+          Log.error(response.error.description)
+        }
+        else {
+          var pollution
           
-					if(self.config.nowCast) {
-						var i=1
-						var pollutions = [null]
-						for (let item of response.query.results.tr) {
-							if((pollution = parseFloat(item.td[self.config.pollutionColumn].replace(',', '.')))) {
-								pollutions[i++] = pollution
-								if(i > 12){
-									break
-								}
-							}
-						}
-						// math from: https://en.wikipedia.org/wiki/NowCast_(air_quality_index)
-						var w = Math.min(pollutions) / Math.max(pollutions)
-						if(self.config.pollutionType != 'O3') {
-							w = w > .5 ? w : .5
-						}
-						var ncl = 0, ncm = 0
-						for (i = 1; i <= 12; i++) {
-							ncl += Math.pow(w, i - 1) * pollutions[i];
-							ncm += Math.pow(w, i - 1)
-						}
-						self.data.pollution = Math.round(100*ncl/ncm)/100
-					}
-					else {
-						for (let item of response.query.results.td) {
-							if((pollution = parseFloat(item.replace(',', '.')))) {
-								self.data.pollution = pollution
-							}
-							else {
-								break
-							}
-						}
-					}
+          if(self.config.nowCast) {
+            var i=1
+            var pollutions = [null]
+            for (let item of response.query.results.tr) {
+              if((pollution = parseFloat(item.td[self.config.pollutionColumn].replace(',', '.')))) {
+                pollutions[i++] = pollution
+                if(i > 12){
+                  break
+                }
+              }
+            }
+            // math from: https://en.wikipedia.org/wiki/NowCast_(air_quality_index)
+            var w = Math.min(pollutions) / Math.max(pollutions)
+            if(self.config.pollutionType != 'O3') {
+              w = w > .5 ? w : .5
+            }
+            var ncl = 0, ncm = 0
+            for (i = 1; i <= 12; i++) {
+              ncl += Math.pow(w, i - 1) * pollutions[i];
+              ncm += Math.pow(w, i - 1)
+            }
+            self.data.pollution = Math.round(100*ncl/ncm)/100
+          }
+          else {
+            for (let item of response.query.results.td) {
+              if((pollution = parseFloat(item.replace(',', '.')))) {
+                self.data.pollution = pollution
+              }
+              else {
+                break
+              }
+            }
+          }
 
-					self.loaded = true;
-					self.updateDom(self.animationSpeed);
-				}
-			});
-		});
-	},
-	html: {
-		icon: '<i class="fa fa-leaf"></i>',
-		city: '<div class="xsmall">{0}</div>',
-		values: '<span class="small light"> ({0} z {1}{2})</span>',
-		quality: '<div>{0} {1}{2}{3}{4}</div>'
-	},
-	getScripts: function() {
-		return [
+          self.loaded = true;
+          self.updateDom(self.animationSpeed);
+        }
+      });
+    });
+  },
+  html: {
+    icon: '<i class="fa fa-leaf"></i>',
+    city: '<div class="xsmall">{0}</div>',
+    values: '<span class="small light"> ({0} ' + this.translate('Of') + ' {1}{2})</span>',
+    quality: '<div>{0} {1}{2}{3}{4}</div>'
+  },
+  getScripts: function() {
+    return [
       '//yui-s.yahooapis.com/3.8.0/build/yui/yui-min.js',
-			'String.format.js'
-		];
-	},
-	getStyles: function() {
-		return ['https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'];
-	},
-	// Override dom generator.
-	getDom: function() {
-		var wrapper = document.createElement('div');
-		if (!this.config.stationID) {
-			wrapper.innerHTML = 'Please set the <i>stationID</i> in the config for module: ' + this.name + '.';
-			wrapper.className = 'dimmed light small';
-		}
-		else if (!this.loaded || !this.data.location) {
-			wrapper.innerHTML = 'Loading air quality ...';
-			wrapper.className = 'dimmed light small';
-		}
-		else if(this.config.nowCast && this.config.pollutionType != 'PM10' && this.config.pollutionType != 'PM2,5' && this.config.pollutionType != 'O3') {
-			wrapper.innerHTML = 'NowCast works only with one of: ' + 'PM10; PM2,5; O3';
-			wrapper.className = "dimmed light small";
-		}
-		else if (!this.data.pollution) {
-			wrapper.innerHTML = 'No data for ' + this.config.pollutionType;
-			wrapper.className = 'dimmed light small';
-		}
-		else {
-			wrapper.innerHTML = 
-				this.html.quality.format(
-					this.html.icon,
-					this.config.showValues ? this.config.pollutionType + ' ' : '',
-					this.impact(this.data.pollution),
-					(this.config.showValues ? this.html.values.format(this.data.pollution.toString().replace('.', ','), this.config.pollutionNorm, this.config.nowCast ? '' : this.config.units) : ''),
-					(this.config.showLocation ? this.html.city.format(this.data.location) : '')
-				)
-		}
-		return wrapper;
-	},
-	impact: function(pollution) {
-		if(pollution < this.config.pollutionNorm) return 'Dobra';
-		else if(pollution < this.config.pollutionNorm * 2) return 'Średnia';
-		else if(pollution < this.config.pollutionNorm * 3) return 'Niska';
-		else if(pollution < this.config.pollutionNorm * 4) return 'Zła';
-		else if(pollution < this.config.pollutionNorm * 6) return 'Bardzo zła';
-		else return 'Toksyczna';
-	},
-	loadLocation: function() {
-		var yql = 'SELECT * FROM html WHERE url=\'' + this.config.url + '\' AND xpath=\'//div[@class="container"]//div[@class="row"]//div[@class="table-responsive"]//table//caption\''
-		var self = this
-		YUI().use('node', 'event', 'yql', function(Y) {
-			Y.YQL(yql, function(response) {
-				if(response.error) {
-					Log.error(response.error.description)
-				}
-				else {
-					self.data.location = response.query.results.caption.replace(/Dane pomiarowe tabele +|\t+|\(.+|\n +/gmi, '')
-					self.updateDom(self.animationSpeed);
-				}
-			});
-		});
-	},
+      'String.format.js'
+    ];
+  },
+  getStyles: function() {
+    return ['https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'];
+  },
+  // Override dom generator.
+  getDom: function() {
+    var wrapper = document.createElement('div');
+    if (!this.config.stationID) {
+      wrapper.innerHTML = this.translate('NoStationID') + this.name + '.';
+      wrapper.className = 'dimmed light small';
+    }
+(??)		else if (!this.loaded) {
+(??)			wrapper.innerHTML = this.translate('Loading');
+      wrapper.className = 'dimmed light small';
+    }
+    else if(this.config.nowCast && this.config.pollutionType != 'PM10' && this.config.pollutionType != 'PM2,5' && this.config.pollutionType != 'O3') {
+      wrapper.innerHTML = this.translate('NowCastErr') + 'PM10; PM2,5; O3';
+      wrapper.className = 'dimmed light small';
+    }
+    else if (!this.data.pollution) {
+      wrapper.innerHTML = this.translate('NoData') + this.config.pollutionType;
+      wrapper.className = 'dimmed light small';
+    }
+    else {
+      wrapper.innerHTML = 
+        this.html.quality.format(
+          this.html.icon,
+          this.config.showValues ? this.config.pollutionType + ' ' : '',
+          this.impact(this.data.pollution),
+          (this.config.showValues ? this.html.values.format(this.data.pollution.toString().replace('.', ','), this.config.pollutionNorm, this.config.nowCast ? '' : this.config.units) : ''),
+          (this.config.showLocation && this.data.location ? this.html.city.format(this.data.location) : '')
+        )
+    }
+    return wrapper;
+  },
+  getTranslations: function() {
+    return {
+        en: 'translations/en.json',
+        pl: 'translations/pl.json'
+    }
+  },
+  impact: function(pollution) {
+         if(pollution < this.config.pollutionNorm)     return this.translate('Good');
+    else if(pollution < this.config.pollutionNorm * 2) return this.translate('Moderate');
+    else if(pollution < this.config.pollutionNorm * 3) return this.translate('Low');
+    else if(pollution < this.config.pollutionNorm * 4) return this.translate('Unhealthy');
+    else if(pollution < this.config.pollutionNorm * 6) return this.translate('VeryUnhealthy');
+    else                                               return this.translate('Hazardous');
+  },
+  loadLocation: function() {
+    var yql = 'SELECT * FROM html WHERE url=\'' + this.config.url + '\' AND xpath=\'//div[@class="container"]//div[@class="row"]//div[@class="table-responsive"]//table//caption\''
+    var self = this
+    YUI().use('node', 'event', 'yql', function(Y) {
+      Y.YQL(yql, function(response) {
+        if(response.error) {
+          Log.error(response.error.description)
+        }
+        else {
+          self.data.location = response.query.results.caption.replace(/Dane pomiarowe tabele +|\t+|\(.+|\n +/gmi, '')
+          self.updateDom(self.animationSpeed);
+        }
+      });
+    });
+  },
 });
--- README.md
+++ README.md
@@ -14,12 +14,12 @@ To run the module, you need to add the following data to your config.js file.
 
 ```
 {
-	module: 'MMM-SmokedMirror',
-	position: 'top_center', // you may choose any location
-	config: {
-		stationID: 488, // the station ID to check the pollution for
-		pollutionType: 'PM2,5' // tupe of the pollution to show
-	}
+  module: 'MMM-SmokedMirror',
+  position: 'top_center', // you may choose any location
+  config: {
+    stationID: 488, // the station ID to check the pollution for
+    pollutionType: 'PM2,5' // tupe of the pollution to show
+  }
 }
 ```
 ### StationID
@@ -27,19 +27,21 @@ Go to your station at [GIOŚ](http://powietrze.gios.gov.pl/pjp/station/search)
 Use the part behind /station_details/info/ for your station.
 For example http://powietrze.gios.gov.pl/pjp/current/station_details/info/544 would be:
 ```
-	stationID: 544
+  stationID: 544
 ```
 
 You may want to set the following options in the config section as well:
 
 | Option |  Description | 
 |---|---|
-| `lang` | change the language<br><br>This is **OPTIONAL**.<br>**Default (and only one for now) value:** `pl` | 
-| `nowCast` | Show [NowCast](https://en.wikipedia.org/wiki/NowCast_(air_quality_index)) instead of hourly data<br><br>* works only with polutionType: `PM10` or `PM2,5` or `O3`<br>**Possible values:** `true` or `false`.<br>**Default value:** `false` | 
-| `showLocation` | toggle location printing<br><br>This is **OPTIONAL**.<br>**Default value:**`true` |
-| `showValues` | toggle values printing<br><br>This is **OPTIONAL**.<br>**Default value:**`true` | 
-| `stationID` | The ID for station you want to show the air quality.<br><br>This is **REQUIRED**.| 
-| `updateInterval` |  change the update period in minutes<br><br>This is **OPTIONAL**.<br>**Default value:** `30` (minutes) | 
+| `animationSpeed` | Speed of the update animation. (Milliseconds)<br><br>**Possible values:** `0` to `5000`<br>**Default value:** `1000` (1 second) | 
+| `lang` | Change the language<br><br>**Possible values:** `en` or `pl` | 
+| `nowCast` | Show [NowCast](https://en.wikipedia.org/wiki/NowCast_(air_quality_index)) instead of hourly data<br><br>* works only with one of polutionType: `PM10` or `PM2,5` or `O3`<br>**Possible values:** `true` or `false`.<br>**Default value:** `false` | 
+| `showLocation` | Toggle location printing<br><br>**Possible values:** `true` or `false`<br>**Default value:** `true` |
+| `showUnits` | Toggle units printing<br><br>* works only with showValues: `true`<br>**Possible values:** `true` or `false`<br>**Default value:** `false` | 
+| `showValues` | Toggle values printing<br><br>**Possible values:** `true` or `false`<br>**Default value:** `false` | 
+| `stationID` | The ID for station you want to show the air quality.<br><br>This is **REQUIRED**.<br>**Possible values:** `PM10`, `PM2,5`, `O3`, `NO2`, `SO2`, `C6H6`, `CO` | 
+| `updateInterval` | How often does the content needs to be fetched? (Minutes)<br><br>**Possible values:** `1` to `144`<br>**Default value:** `30` (30 minutes) | 
 
 ### Known Issues
 
